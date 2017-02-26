0228e37cb411bc8c1bfea98de808ac106ea7b70a
vuito76 <gkhabas76@gmail.com>
2017-02-26 07:43:58 +0545

    new file

---
+++ variabletypes.js
@@ -0,0 +1,51 @@
+//There are different variable types in javascript.
+
+// 1) Numbers 
+//Below are some examples of numbers variables
+var one = 11
+console.log("The total value is: " + one)
+// document.write(one)
+// alert(one)
+
+var two = 11+13
+console.log("The total added value is: " + two)
+// document.write("The total is: " + two)
+// alert("The total is: " + two)
+
+var three = 11-13
+console.log("The total subtracted value is: " + three)
+
+var four = 11*13
+console.log("The total multiplication value is: " + four)
+
+var five = 11/13
+console.log("The total division value is: " + five)
+
+// 2) Boolean (Boolean variable type represents "True" or "False" vlues)
+//Below are some examples of boolean variables
+
+var correct = true
+console.log("The first boolean value is: " + correct)
+
+var incorrect = false
+console.log("The second boolean value is: " + incorrect)
+
+// 3) String
+//Below are some examples of string variables(Strings should always be in double quotation marks i.e "name")
+
+var firstname = "Govinda"
+var familyname = "Khabas"
+console.log("Your first name is: " + firstname)
+console.log("Your family name is: " + familyname)
+
+// 4) Array
+//Below are some examples of array variables
+
+var numbers = [11, 12, 13, 14]
+console.log("The first array are: " + numbers)
+
+var boolean = [true, false]
+console.log("The second array are: " + boolean)
+
+var strings = ["Hello Beautiful", "I love Your"]
+console.log("The third array are: " + strings)
